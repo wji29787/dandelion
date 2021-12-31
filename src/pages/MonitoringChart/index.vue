ad67986cd3072002abc5793842244d6a455afb0b
吴震 <wuzhen@cpgroup.cn>
2021-12-31 11:53:08 +0800

    add 时间轴

--- src/pages/MonitoringChart/index.vue
+++ src/pages/MonitoringChart/index.vue
@@ -29,7 +29,7 @@
         
       <template v-if="formData.model==='gsyt'">
          <a-form-item label="时间">
-          <a-input placeholder="" />
+          <a-date-picker show-time placeholder="时间" />
         </a-form-item>
          <a-form-item label="泄露密度">
           <a-input placeholder="单位mg/m" />
@@ -79,8 +79,26 @@
         <li><img :src="pics.normal" alt="Picture 1"></li>
         <li><img :src="pics.bzy" alt="Picture 2"></li>
         <li><img :src="pics.gsyt" alt="Picture 3"></li>
+        <li><img :src="pics.gsytlater" alt="Picture 3"></li>
      </ul>
     </div>
+    <!--时间轴-->
+     <div class="time-box" v-if="showSlider">
+       <a-form>
+          <a-form-item  :colon="false">
+            <template #label>
+              <span :style="{color:'#FFF'}">时间轴</span>
+            </template>
+            
+           <a-slider v-model:value="timeSLider" :min="0" :max="4"  :marks="masks" @change="timeSLiderChange">
+              <template #mark="{label}">
+              <span :style="{color:'#FFF'}">{{label}}</span>
+            </template> 
+
+           </a-slider >
+        </a-form-item>
+       </a-form>
+     </div>
     <!-- <img id="image" style="display: none;" src="@/images/map.jpg" alt="Picture" /> -->
     <div class="right">
       <!-- <div class="title">请输入坐标或者地图中点击</div> -->
@@ -88,37 +106,37 @@
         <a-form-item label="侦查方案">
           <a-button >详情>></a-button>
         </a-form-item>
-        <a-form-item label="装备">
-          <!-- <a-input /> -->
-        </a-form-item>
-      </a-form>
-      <a-form>
-        <a-form-item label="数量">
-        </a-form-item>
-        <a-form-item label="目标物">
+        <!-- <a-form-item label="装备">
+     
+        </a-form-item> -->
+      <!-- </a-form> -->
+      <!-- <a-form> -->
+        <!-- <a-form-item label="数量">
+        </a-form-item> -->
+        <!-- <a-form-item label="目标物">
 
-        </a-form-item>
-        <a-form-item label="地点">
-        </a-form-item>
+        </a-form-item> -->
+        <!-- <a-form-item label="地点">
+        </a-form-item> -->
         <a-form-item label="人员方案">
          <a-button >详情>></a-button>
         </a-form-item>
-        <a-form-item label="装备">
+        <!-- <a-form-item label="装备">
 
-        </a-form-item>
-        <a-form-item label="使用时长">
-        </a-form-item>
+        </a-form-item> -->
+        <!-- <a-form-item label="使用时长">
+        </a-form-item> -->
          <a-form-item label="消洗方案">
 
          <a-button >详情>></a-button>
         </a-form-item>
-        <a-form-item label="装备">
+        <!-- <a-form-item label="装备">
 
-        </a-form-item>
-        <a-form-item label="消息地">
-        </a-form-item>
-        <a-form-item label="地点">
-        </a-form-item>
+        </a-form-item> -->
+        <!-- <a-form-item label="消息地">
+        </a-form-item> -->
+        <!-- <a-form-item label="地点">
+        </a-form-item> -->
       </a-form>
     </div>
   </div>
@@ -139,6 +157,7 @@ import  normal from './img/normal.jpg';
 
 const pics = {
   gsyt:gsytys,
+  gsytlater:gsytlater,
   bzy:bzy,
   normal:normal,
 }
@@ -146,17 +165,44 @@ let viewIns = null
 const formData = reactive({
       model:''
 })
+const timeSLider = ref(0);
+const showSlider = ref(false)
+const masks = ref({
+  0:'0h',
+  1:'1',
+  2:'2',
+  3:'3',
+  4:'4h',
+})
 const createPic = ()=>{
      console.log('formData.model',formData.model)
     if(formData.model){
       if(formData.model==='gsyt'){
-         viewIns.view(2)
+        //  viewIns.view(2)
+
+        timeSLider.value = 0;
+        showSlider.value = true
+        timeSLiderChange(0)
+        return;
       }else {
          viewIns.view(1)
       }
     }else {
       viewIns.view(0)
     }
+    showSlider.value = false
+}
+const timeSLiderChange = (val)=>{
+   const pot = {
+     0:2,
+     1:2,
+     2:3,
+     3:3,
+     4:3
+   }
+   if(pot[val]){
+      viewIns.view(pot[val])
+   }
 }
 onMounted(() => {
   // View an image.
@@ -203,6 +249,16 @@ onUnmounted(()=>{
    .left{
     // overflow: scroll;
     padding-bottom: 170px;
+  }
+  .time-box{
+     position: absolute;
+     z-index: 999;
+     width:300px;
+     height: 30px;
+     top:30px;
+     left:50%;
+     transform: translateX(-50%);
+
   }
   // .left {
   //   position: relative;
